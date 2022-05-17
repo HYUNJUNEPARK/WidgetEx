3ce49eea646bfdc8b7a39b50ff677672cfe65b99
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-05-17 17:17:25 +0900

    finish

--- app/src/main/java/com/raywenderlich/android/coffeelogs/activity/WidgetCoffeeLogActivity.kt
+++ app/src/main/java/com/raywenderlich/android/coffeelogs/activity/WidgetCoffeeLogActivity.kt
@@ -2,12 +2,31 @@ package com.raywenderlich.android.coffeelogs.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.raywenderlich.android.coffeelogs.R
+import android.view.View
+import com.raywenderlich.android.coffeelogs.databinding.ActivityWidgetCoffeeLogBinding
+import com.raywenderlich.android.coffeelogs.preferences.CoffeeLogPreferences
+import com.raywenderlich.android.coffeelogs.widget.UpdateWidget
 
 class WidgetCoffeeLogActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityWidgetCoffeeLogBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_widget_coffee_log)
+        setContentView(binding.root)
+    }
 
+    fun initCloseButton(v: View) {
+        finish()
+    }
+
+    fun initConfirmButton(v: View) {
+        val coffeeLogPreferences = CoffeeLogPreferences(this)
+        val todayCoffee = coffeeLogPreferences.getTodayCoffeePref()
+        val userInput: String = binding.userInputEditText.text.toString()
+        val totalCoffee = todayCoffee + userInput.toInt()
+        coffeeLogPreferences.saveTodayCoffeePref(totalCoffee)
+        val updateWidget = UpdateWidget()
+        updateWidget.updateWidget(this)
+        finish()
     }
 }
