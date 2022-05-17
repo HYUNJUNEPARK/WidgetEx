46085111a25917b0c207e970122d62d628b9a372
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-05-17 16:49:16 +0900

    widget Layout

---
+++ app/src/main/java/com/raywenderlich/android/coffeelogs/activity/WidgetCoffeeLogActivity.kt
@@ -0,0 +1,13 @@
+package com.raywenderlich.android.coffeelogs.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.raywenderlich.android.coffeelogs.R
+
+class WidgetCoffeeLogActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_widget_coffee_log)
+
+    }
+}
--- app/src/main/java/com/raywenderlich/android/coffeelogs/widget/UpdateWidget.kt
+++ app/src/main/java/com/raywenderlich/android/coffeelogs/widget/UpdateWidget.kt
@@ -9,12 +9,17 @@ import android.os.Build
 import android.widget.RemoteViews
 import com.raywenderlich.android.coffeelogs.R
 import com.raywenderlich.android.coffeelogs.activity.MainActivity
-import com.raywenderlich.android.coffeelogs.constant.CoffeeTypes
+import com.raywenderlich.android.coffeelogs.activity.WidgetCoffeeLogActivity
 import com.raywenderlich.android.coffeelogs.constant.Constants
+import com.raywenderlich.android.coffeelogs.constant.Constants.Companion.ADD_COFFEE_INTENT
+import com.raywenderlich.android.coffeelogs.constant.Constants.Companion.GRAMS_EXTRA
+import com.raywenderlich.android.coffeelogs.constant.Constants.Companion.SET_BACKGROUND_RES
 import com.raywenderlich.android.coffeelogs.preferences.CoffeeLogPreferences
 import com.raywenderlich.android.coffeelogs.receiver.CoffeeLogReceiver
 
 class UpdateWidget {
+    private lateinit var configPendingIntent: PendingIntent
+
     fun updateWidget(context: Context) {
         val appWidgetManager = AppWidgetManager.getInstance(context)
         val appWidgetIds = appWidgetManager.getAppWidgetIds(ComponentName(context, CoffeeLogWidget::class.java))
@@ -27,45 +32,22 @@ class UpdateWidget {
         val coffeeLogPreferences = CoffeeLogPreferences(context)
         val todayCoffee = coffeeLogPreferences.getTodayCoffeePref().toString()
 
+        val intent = Intent(context, WidgetCoffeeLogActivity::class.java)
+        configPendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT)
+
         // Construct the RemoteViews object
         val views = RemoteViews(context.packageName, R.layout.coffee_logger_widget)
         views.run {
             setTextViewText(R.id.appwidget_text, todayCoffee)
             setTextViewText(R.id.limitTextView, coffeeLogPreferences.getLimitPref().toString())
 
-            //activity
-            setOnClickPendingIntent(
-                R.id.ristretto_button,
-                getActivityPendingIntent(context, CoffeeTypes.RISTRETTO.grams)
-            )
-            setOnClickPendingIntent(
-                R.id.espresso_button,
-                getActivityPendingIntent(context, CoffeeTypes.ESPRESSO.grams)
-            )
-            setOnClickPendingIntent(
-                R.id.long_button,
-                getActivityPendingIntent(context, CoffeeTypes.LONG.grams)
-            )
-
-            //receiver
-            setOnClickPendingIntent(
-                R.id.ristretto_service_button,
-                getBroadcastPendingIntent(context, CoffeeTypes.RISTRETTO.grams)
-            )
-            setOnClickPendingIntent(
-                R.id.espresso_service_button,
-                getBroadcastPendingIntent(context, CoffeeTypes.ESPRESSO.grams)
-            )
-            setOnClickPendingIntent(
-                R.id.long_service_button,
-                getBroadcastPendingIntent(context, CoffeeTypes.LONG.grams)
-            )
+            setOnClickPendingIntent(R.id.widget_add_button, configPendingIntent)
         }
 
         //update widget color by limit
         val limitCoffee: Int = coffeeLogPreferences.getLimitPref()
         val backgroundColor = if (limitCoffee < todayCoffee.toInt()) R.drawable.background_overlimit else R.drawable.background
-        views.setInt(R.id.widget_layout, Constants.SET_BACKGROUND_RES, backgroundColor)
+        views.setInt(R.id.widget_layout, SET_BACKGROUND_RES, backgroundColor)
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views)
@@ -73,8 +55,8 @@ class UpdateWidget {
 
     private fun getActivityPendingIntent(context: Context, grams: Int): PendingIntent {
         val intent = Intent(context, MainActivity::class.java)
-        intent.action = Constants.ADD_COFFEE_INTENT
-        intent.putExtra(Constants.GRAMS_EXTRA, grams)
+        intent.action = ADD_COFFEE_INTENT
+        intent.putExtra(GRAMS_EXTRA, grams)
         return PendingIntent.getActivity(
             context,
             grams,
